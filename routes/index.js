566af5cc7a57e8c1f8ae097b9b3a16e06ae48e16
Axel <eldrank@gmail.com>
2022-11-26 10:28:27 +0100

    Add input page and create array to store value

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date().toLocaleString().split(',')[0]
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toLocaleString().split(',')[0]
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages });
+});
+
+//GET new message page
+router.get('/new', (req, res) => {
+  res.render('new', { title: 'New message: '});
+})
+
+router.post('/new', (req, res) => {
+  const message = {
+    user: req.body.user,
+    text: req.body.text,
+    added: new Date().toLocaleString().split(',')[0]
+  }
+  messages.push(message);
+  res.redirect('/');
 });
 
 module.exports = router;
